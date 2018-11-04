b4935fd28f5901166a622ce855b54e1169a1effa
0rr <sethwbrown22@gmail.com>
2018-11-04 18:51:15 +0000

    SPA_19

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -2,7 +2,9 @@
 		//the taskhtml methos takes in a JS representation of the task and pproduces an HTML representation using <li> tags
 		function taskHtml(task){
 			var checkedStatus = task.done ? "checked" : "";
-			var liElement = '<li><div class="view"><input class="toggle" type="checkbox"' + 
+			var liClass = task.done ? "completed" : "";
+			var liElement = '<li id="listItem-' + task.id + '" class= "' + liClass + '">' + 
+			'<div class="view"><input class="toggle" type="checkbox"' + 
 			"data-id= '" + task.id + "' " +
 			checkedStatus + 
 			'><label>' +
@@ -23,6 +25,11 @@
 					task: {
 						done: doneValue
 					}
+				}).success(function(data){
+					var liHtml = taskHtml(data);
+					var $li = $("#listItem-" + data.id);
+					$li.replaceWith(liHtml);
+					$('.toggle').change(toggleTask);
 				});
 		}
 
